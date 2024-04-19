efbe0a5892fad947025f9e7d90ea88798943cf2f
caesar <caesarlarsson0@gmail.com>
2024-04-19 15:54:11 +0200

    ye

---
+++ def.py
@@ -0,0 +1,10 @@
+def introduction(first_name, last_name="Smith"):
+    print("Hello, my name is", first_name, last_name)
+
+introduction("James", "Doe")
+introduction("Henry")
+introduction(first_name="William")
+introduction(first_name=input("Enter your first name: "))
+
+
+
